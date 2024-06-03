3e708b9f310c60814c5a21e9af83feb9b3f058ec
Marie Le Lamer <lelamer.marie17@gmail.com>
2024-06-03 23:00:57 +0200

    :iphone: responsive page ok - responsive card a faire

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -35,11 +35,13 @@ export default function MoviePage() {
 
     return (
         <div>
-            <header className="fixed bg-awesome-yellow w-full justify-center text-3xl font-bold font-MonografTextBold z-20">MOVIE NIGHT</header>
-            <div className="flex flex-col gap-4 justify-center items-center pt-12">
+            <header className="fixed bg-awesome-yellow w-full justify-center text-3xl font-bold font-MonografTextBold z-20 flex text-center items-center p-3 lg:w-1/4 lg:h-full lg:flex-col">
+                <img src="film-svgrepo-com.svg" alt="film icon" className="w-8 mr-3 lg:w-16"/>
+                MOVIE NIGHT</header>
+            <div className="grid grid-cols-1 gap-4 justify-center items-center pt-20 auto-rows-auto lg:grid-cols-2 lg:pt-5 lg:ml-[25%]">
                 {data.length > 0
                     ? data.map((movie, index) => (
-                          <div key={index}  className="bg-beautiful-grey backdrop-blur-sm justify-center items-center w-4/5 rounded">
+                          <div key={index}  className="bg-beautiful-grey backdrop-blur-sm justify-center items-center w-4/5 rounded mx-auto h-full">
                               {" "}
                               <Card poster={movie.poster} title={movie.title} year={movie.year} rating={movie.rating} genre={movie.genre} director={movie.director} />
                           </div>
