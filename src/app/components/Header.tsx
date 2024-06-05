1d594c66e7981e0aa5c9f30b7e1376bd52570eb4
Laporte Andoni <lapand@hotmail.fr>
2024-06-05 08:27:17 +0200

    sm header z-index fixed

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Header: React.FC = () => {
   return (
-    <header className="fixed bg-primary flex justify-center items-center md:flex-col gap-8 text-center h-28 md:h-screen md:p-10 w-full md:w-1/4 md:min-w-[300px]">
+    <header className="fixed z-20 bg-primary flex justify-center items-center md:flex-col gap-8 text-center h-28 md:h-screen md:p-10 w-full md:w-1/4 md:min-w-[300px]">
         <div className="w-12 md:w-[25%] md:max-w-[70px]">
             <Image src="/video-logo.svg" alt="video-logo" width="200" height="200" />
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ const Home: React.FC = () => {
   return (
     <>
       <Header />
-      <main className="flex max-md:flex-col md:flex-wrap items-center max-xl:justify-center xl:justify-end gap-10 md:gap-8 p-8 max-md:mt-20 md:ml-[25%]">
+      <main className="flex max-md:flex-col md:flex-wrap items-center justify-end gap-10 md:gap-8 p-8 max-md:mt-20 md:ml-[25%]">
         <div className="main-bg max-h-screen"></div>
         <Movies />
       </main>
